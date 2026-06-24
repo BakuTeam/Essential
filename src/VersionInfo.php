f2602ed3fd77e46eecf757c1550002f802e413f6
sheluvmyshe <mikhailhrushev@gmail.com>
2026-06-25 01:49:25 +0300

    rebrand: PocketMine-MP -> Dummy, set base version 5.39.1

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -30,8 +30,8 @@ use function is_int;
 use function str_repeat;
 
 final class VersionInfo{
-	public const NAME = "PocketMine-MP";
-	public const BASE_VERSION = "5.40.0";
+	public const NAME = "Dummy";
+	public const BASE_VERSION = "5.39.1";
 	public const IS_DEVELOPMENT_BUILD = true;
 	public const BUILD_CHANNEL = "stable";
 
